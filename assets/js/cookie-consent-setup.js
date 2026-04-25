837e67638f808e93f4ae42f7652c003833a22959
xingbod <xingbod@gmail.com>
2026-04-25 08:45:05 +0000

    Deploying to gh-pages from @ xingbod/x@ebf02051a48f6f8c95b314f87b6536d7358614b0 🚀

--- assets/js/cookie-consent-setup.js
+++ assets/js/cookie-consent-setup.js
@@ -96,7 +96,7 @@ function initializeCookieConsent() {
               },
               {
                 title: 'More information',
-                description: 'For any queries in relation to our policy on cookies and your choices, please <a class="cc-link" href="https://www.drdongai.com/al-folio/#contact">contact us</a>.'
+                description: 'For any queries in relation to our policy on cookies and your choices, please <a class="cc-link" href="https://www.drdongai.com/x/#contact">contact us</a>.'
               }
             ]
           }
